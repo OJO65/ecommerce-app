9936ebce6814e6304adc154b413ab55ff0b321aa
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-08-24 11:18:40 +0300

    chore(env): Firebase configuration for dev and prod
    Created environment.ts and environment.prod.ts files:
    
    - environment.ts:
      • production: false
      • Contains Firebase project configuration for development
    
    - environment.prod.ts:
      • production: true
      • Contains Firebase project configuration for production
    
    Angular CLI replaces environment.ts with environment.prod.ts during
    production builds, ensuring the correct Firebase settings are used
    depending on the build target.

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: 'firebase-ecommerce-store',
+    appId: '1:35386830329:web:d6c6fbec9be30f7a09a128',
+    storageBucket: 'firebase-ecommerce-store.firebasestorage.app',
+    apiKey: 'AIzaSyAz6LCOIANZj1Zh9sig9J6ZqP5l3K40A_k',
+    authDomain: 'fir-ecommerce-store-25487.firebaseapp.com',
+    messagingSenderId: '35386830329',
+  }
+};
