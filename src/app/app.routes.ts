27ace26124804c092da4ec181b26fcf55f61cb00
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-08-26 15:42:43 +0300

    feat: added register and forgot-password pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './core/features/auth/login/login.component';
+import { RegisterComponent } from './core/features/auth/register/register.component';
+import { ForgotPasswordComponent } from './core/features/auth/forgot-password/forgot-password.component';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent }
 ];
