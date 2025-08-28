d10a39552306c4206fde964f8e9b57892e98bb65
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-08-28 14:54:23 +0300

    feat(app): auth page detection and update app title
    - Introduced isAuthPage() helper to hide header on login/register/forgot-password pages
    - Imported HeaderComponent and CommonModule for global layout
    - Updated app title signal from "ecommerce-app" to "e-kabanda"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { HeaderComponent } from "./components/header/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ecommerce-app');
+  protected readonly title = signal('e-kabanda');
+  constructor(private router: Router) {}
+
+  isAuthPage(): boolean {
+    const authPages = ['/login', 'register', 'forgot-password'];
+    return authPages.includes(this.router.url);
+  }
 }
